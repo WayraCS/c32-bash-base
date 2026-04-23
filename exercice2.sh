69abc545b1eaa354b9b885b5ffc2b5f861cad873
Red <x@gmail.com>
2026-04-23 10:21:19 -0400

    exercice2 completed

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+
+while [[ $choix != q ]];do 
+    clear
+    echo "------------------------------"
+    echo "      MENU VERIFICATION       "
+    echo "      m = dictionnaire        "
+    echo "      q = quitter             "
+    echo "------------------------------"
+    read -p "Entrez un choix: " choix
+
+    if [[ $choix = m ]];then
+        read -p "Entrez un mot: " mot
+        ./exercice1.sh $mot
+        sleep 3
+    fi
+done
