a9c01a46ee326d2381fefb5811bea91ba3565dae
Red <x@gmail.com>
2026-04-23 09:40:37 -0400

    boucles notions

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++));do 
+    echo $i
+done
+
+
+ls 
+listeFichier=`ls`
+
+
+for fichier in $listeFichier;do
+    echo $fichier
+done
+
+
+while [[ $choix != q ]];do
+    echo "========="
+    echo "- Menu 1 -"
+    echo "-----------"
+    echo ""
+    read -p "Entrez un choix : " choix
+done
