0204d8c80e5ba368fb9830b1f2d862df56de0c24
Red <x@gmail.com>
2026-04-23 09:07:14 -0400

    variable notions

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 # pour executer bash ./ ou encore / (a partir de la racine)
 
-
-
 echo "BOOM"
-echo -n "Vive "
+echo -n "Vive "                         # -n = pas de saut de ligne
 echo "Linux"   
-echo -e "123\n\t456"
-
-# pas de $ quand on affecte
-# en lecture de la variable on met $ en avant
-read -p "Quel age avez-vous? " age
+echo -e "123\n\t456"                    # cela lis les \n \t etc..
+read -p "Quel age avez-vous? " age      # pas de $ quand on affecte # en lecture de la variable on met $ en avant
 echo $age "ans!"
 
 # exit
--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+prenom=John
+
+echo $prenom
+
+
+
+declare -i age=44       # pour mettre un int dans une variable
+declare -r nom=Smith    #lecture seule (constante)
+echo $age
+echo $0                 #Nom du fichier
+echo $1                 #Parametre 1
+echo $#                 #Combien de parametre ont ete passes
