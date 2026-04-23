c67e6ca137734b41d6759ac0298ab83fa0a2ee60
Red <x@gmail.com>
2026-04-23 08:58:52 -0400

    Echo + read notions

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+# pour executer bash ./ ou encore / (a partir de la racine)
+
+
+
+echo "BOOM"
+echo -n "Vive "
+echo "Linux"   
+echo -e "123\n\t456"
+
+# pas de $ quand on affecte
+# en lecture de la variable on met $ en avant
+read -p "Quel age avez-vous? " age
+echo $age "ans!"
+
+# exit
 
