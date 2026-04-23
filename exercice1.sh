f80ea93bee32c585a97a57de98673cdfc87e0633
Red <x@gmail.com>
2026-04-23 10:11:34 -0400

    exercice1 completed

--- exercice1.sh
+++ exercice1.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-# La ligne suivante permet de vérifier si un mot existe dans le dictionnaire
-# Il faut suffixer cet URL avec le mot entré par l'uasger (ex: word=chat)
-#wget -qO - http://dictionary.objectif8.com/exists.php?word=
+# La ligne suivante permet de vÃ©rifier si un mot existe dans le dictionnaire
+# Il faut suffixer cet URL avec le mot entrÃ© par l'uasger (ex: word=chat)
+#wget -qO - https://dictionary.objectif8.com/exists.php?word=
+
+mot=$1
+resultat=`wget -qO - https://dictionary.objectif8.com/exists.php?word=$1`
+
+
+if [[ $resultat = 1 ]];then
+    echo "Existe"
+else 
+    echo "Non existant"
+fi
